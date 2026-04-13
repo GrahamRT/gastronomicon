2ac03174b97b992338ce3db5b947e5399c15d758
GEE <gm.walker2022@gmail.com>
2026-04-13 16:20:11 -0400

    add stores
    - add searchData (records queries made by the user)
    - add ingredients (stores the data retrieved from the api)

--- app/src/lib/stores.ts
+++ app/src/lib/stores.ts
@@ -1,3 +1,17 @@
-import { writable } from 'svelte/store';
-
+import { writable, derived } from 'svelte/store';
+import { getIngredient, getPairings } from '$lib/util';
 export const searchData = writable<string[]>([]);
+
+export const ingredients = derived(searchData, ($searchData, set) => {
+  const loadIngredients = async () =>{
+    if($searchData.length === 0) {
+      set([]);
+      return;
+    }
+    const requests = $searchData.map(getIngredient);
+    const results = await Promise.all(requests);
+    console.log(results);
+    set(results.flat());
+  };
+  loadIngredients();
+});
