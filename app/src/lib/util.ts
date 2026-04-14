ecd7e1e871bd0dab22caec151c6668e032c8cacd
GEE <gm.walker2022@gmail.com>
2026-04-14 17:03:43 -0400

    ch: remove logging

--- app/src/lib/util.ts
+++ app/src/lib/util.ts
@@ -1,13 +1,11 @@
 export async function getIngredient(ingredients: string) {
   let response = await fetch(`/api/ingredients?name=${encodeURIComponent(ingredients)}`);
   let result = await response.json();
-  console.log(result);
   return result;
 }
 
 export async function getPairings(ingredients: string) {
   let response = await fetch(`/api/pairings?name=${encodeURIComponent(ingredients)}`);
   let result = await response.json();
-  console.log(result);
   return result;
 }
